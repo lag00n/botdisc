4c57d41f723d0ace94e009bb8e85c49b84338726
lxg00n <AmanoKeita25@gmail.com>
2021-03-10 15:12:39 +0000

    trying some new things

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ## feito pelo murilao boladao. github.com/lag00n
 
-import discord 
+import discord
 from discord.ext import commands, tasks
 import hostingsetup
 import info
@@ -10,75 +10,89 @@ import random
 key = os.getenv('key')
 wkey = os.getenv('wkey')
 
-client = commands.Bot( # deveria trocar o nome pra bot? ao invés de client.
-  command_prefix = 'mu!',
-  case_insensitive=True
-  )
+client = commands.Bot(  # deveria trocar o nome pra bot? ao invés de client.
+    command_prefix='mu!',
+    case_insensitive=True)
 
 client.author_id = 740276491094327356
 client.remove_command('help')
+
 
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Game(name="iasmin na minha cama"))
     print('Conectado com sucesso como:', client.user)
 
+
 @client.command()
 async def iasmin(ctx):
     await ctx.send(random.choice(things.iasmin))
 
+
 @client.command()
-async def say(ctx, *, arg: str): ## * e arg:str permitem o não uso de aspas.
-  await ctx.send(arg)
+async def say(ctx, *, arg: str):  ## * e arg:str permitem o não uso de aspas.
+    await ctx.send(arg)
+
 
 @client.command()
 async def revsay(ctx, *, arg: str):
-  await ctx.send (arg[::-1])
+    await ctx.send(arg[::-1])
+
 
 @client.command()
 async def moeda(ctx):
-  rand_int = random.randint(0, 1)
-  if rand_int == 0:
-    resultado = "Cara"
-  else:
-    resultado = "Coroa"
-  await ctx.send(resultado)
+    rand_int = random.randint(0, 1)
+    if rand_int == 0:
+        resultado = "Cara"
+    else:
+        resultado = "Coroa"
+    await ctx.send(resultado)
+
 
 @client.command(name='erase', help='esse commando apaga mensagens')
-async def erase(ctx, amount = 10 + 2):
-  await ctx.send("Apagando Mensagens")
-  await ctx.channel.purge(limit=amount)
+async def erase(ctx, amount=10 + 2):
+    await ctx.send("Apagando Mensagens")
+    await ctx.channel.purge(limit=amount)
+
 
 @client.command()
 async def warn(ctx, member: discord.Member, *, msg):
-  channel = await member.create_dm()
-  await channel.send(msg)
+    channel = await member.create_dm()
+    await channel.send(msg)
 
-count = 0
+
 @client.command()
 async def flood(ctx, member: discord.Member, *, msg):
-  channel = await member.create_dm()
-  await channel.send(msg)
-  while count < int(90):
+    count = 0
+    channel = await member.create_dm()
     await channel.send(msg)
-count = count + 1
+    while (count <= 10):  # ou (count <= 10)
+        await channel.send(msg)
+        if count == 10:
+            break
+        count += 1
+
 
 @client.command()
 async def embed(ctx):
-    embed=discord.Embed(title="Exemplo Embed", url="https://google.com/", description="Esse é um exemplo de embed.", color=0xFF5733)
+    embed = discord.Embed(title="DELBEN CLICA AQUI",
+                          url="https://xvideos.com/",
+                          description="Delben clica ali .",
+                          color=0xFF5733)
     await ctx.send(embed=embed)
+
 
 @client.event
 async def on_message(message):
 
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  if "delben" in message.content:
-    await message.channel.send("delben macaco wtf :monkey:")
-     
-  await client.process_commands(message) # desbuga os outros commandos.
+    if "delben" in message.content:
+        await message.channel.send("delben macaco wtf :monkey:")
 
- 
+    await client.process_commands(message)  # desbuga os outros commandos.
+
+
 hostingsetup.host()
 client.run(os.getenv('TOKEN'))
